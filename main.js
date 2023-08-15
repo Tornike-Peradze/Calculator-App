30b4128bff3319fc61297d6426e3ca542a3c97ec
Tornike-Peradze <t_peradze@cu.edu.ge>
2023-08-15 10:01:46 +0400

    JS part started

---
+++ main.js
@@ -0,0 +1,10 @@
+let slider = document.querySelector(".slider");
+let resultBar = document.querySelector("#res-n");
+let components = document.querySelector(".button");
+let deleteBtn = document.querySelector(".delete");
+let resetBtn = document.querySelector(".reset");
+let equalBtn = document.querySelector(".equal");
+
+let resulted = 0;
+
+slider.addEventListener("click", function () {});
